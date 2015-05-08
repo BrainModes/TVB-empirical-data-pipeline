c2d45de407e807aea722521f56ae5b11594d1ec7
Simon R <srothmei@mailbox.tu-berlin.de>
2015-05-08 19:32:00 +0200

    SLURM parallel processing
    Modified the GNU parallel call to match SLURMs node addressing.
    Parallel will now spawn several jobs across different nodes via
    ssh-login

--- runTracking.sh
+++ runTracking.sh
@@ -3,4 +3,12 @@
 #This files performs the execution of xargs to compute the fiber tracking
 #in parallel
 #cat ./batch_track.sh | xargs -n 4 -P 0 ./trackingClusterDK.sh
-cat ./batch_track.sh | parallel --colsep ' ' -j +0 ./trackingClusterDK.sh
+
+#Init all Toolboxes
+source ./pipeSetup.sh
+
+#Get the hostlist
+hostlist = $(scontrol show hostname $SLURM_NODELIST | paste -d, -s)
+
+#Run the jobs
+parallel -C ' ' --sshlogin $hostlist --delay .2 -j $SLURM_CPUS_ON_NODE --workdir $(pwd) --joblog runtask.log --resume ./trackingClusterDK.sh {1} {2} {3} {4} < ./batch_track.sh
